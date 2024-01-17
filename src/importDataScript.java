d0f60b87c4c62e8ef87f25c27adc08457d161265
rhit-chungea <chungea@rose-hulman.edu>
2024-01-17 14:39:52 -0500

    started

---
+++ src/importDataScript.java
@@ -0,0 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class importDataScript {
+
+    public static void main(String[] args) {
+        String url = "jdbc:sqlserver://${dbServer};databaseName=${dbName};user=${user};password={${pass}};encrypt=false";
+
+
+        String fullURL = url
+                .replace("${dbServer}", "golem.csse.rose-hulman.edu")
+                .replace("${dbName}", "Hospital")
+                .replace("${user}", "hospitalAdmin")
+                .replace("${pass}", "Password123");
+
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(fullURL);
+            System.out.println("Success");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            if(connection != null && !connection.isClosed()){
+                connection.close();
+            }
+        } catch (SQLException e) {
+            //do nothing
+        }
+    }
+
+}
+
